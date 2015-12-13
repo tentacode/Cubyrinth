303506637f7c982478ffbe9119c4bae78879632a
Gabriel Pillet <contact@gabrielpillet.com>
2015-12-13 14:12:13 +0100

    DeathByFall

---
+++ Assets/Scripts/Behaviour/Player/DeathByFall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathByFall : MonoBehaviour
+{
+    public float deathHeight;
+    
+    private CharacterController characterController;
+    private enum FallState {Grounded, Falling};
+    private FallState fallState = FallState.Grounded;
+    private Vector3 lastGroundedPosition;
+
+	void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+	
+	void Update ()
+    {
+        if (!characterController.isGrounded && fallState == FallState.Grounded) {
+            lastGroundedPosition = transform.position;
+            fallState = FallState.Falling;
+        }
+
+        if (characterController.isGrounded && fallState == FallState.Falling)
+        {
+            fallState = FallState.Grounded;
+            float fallHeight = lastGroundedPosition.y - transform.position.y;
+
+            if (fallHeight > deathHeight)
+            {
+                Debug.Log("Death by fall");
+                GetComponent<Death>().Die();
+            }
+        }
+    }
+}
