dda551037d6b4168a0ef1e8f04ae50524e5aad75
Gabriel Pillet <contact@gabrielpillet.com>
2015-12-13 14:24:13 +0100

    Exit game on escape keypress

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	void Update ()
+    {
+	    if (Input.GetButtonUp("Cancel"))
+        {
+            Application.Quit();
+        }
+	}
+}
