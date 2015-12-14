4c85f7e9f7d51b886a161cfb5cafdb58d967f2bc
Gabriel Pillet <contact@gabrielpillet.com>
2015-12-14 21:46:17 +0100

    Custom Rotator

--- Assets/Scripts/Movement/RotateProps.cs
+++ Assets/Scripts/Movement/RotateProps.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class RotateProps : MonoBehaviour
 {
+    float rotationSpeed = 25.0f;
+
     void Start()
     {
-        iTween.RotateBy(gameObject, iTween.Hash("x", 1, "time", 10, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+        //iTween.RotateBy(gameObject, iTween.Hash("x", 1, "time", 10, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
     }
 }
 
