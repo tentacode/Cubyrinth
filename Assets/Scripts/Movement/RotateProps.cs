e6a2af153e6f24e0a52f90bb3c67b3a83ef32d83
Gabriel Pillet <contact@gabrielpillet.com>
2015-12-14 12:48:31 +0100

    Fixing Labyrinth Scene, proper RotateProps, removing Plugins folder

---
+++ Assets/Scripts/Movement/RotateProps.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateProps : MonoBehaviour
+{
+    void Start()
+    {
+        iTween.RotateBy(gameObject, iTween.Hash("x", 1, "time", 10, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+    }
+}
+
